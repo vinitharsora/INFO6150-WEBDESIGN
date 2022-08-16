0f262ca7caa5069a89ff517d7cd1358a373311ee
Vinit Harsora <harsora.v@northeastern.edu>
2022-08-16 19:51:53 -0400

    A10

---
+++ src/HourlyCityForecast.js
@@ -0,0 +1,46 @@
+/* Assignment 10 */
+import React from "react";
+
+var moment = require("moment");
+
+function HourlyForecast(props) {
+
+  console.log("Props ", props);
+
+  let _date = new Date();
+  const weekday = props.data.dt * 1000;
+  _date.setTime(weekday);
+  const _img = `owf owf-${props.data.weather[0].id} owf-5x`;
+  const fahrenheitMax = Math.round(props.data.main.temp_max);
+
+  const fahrenheitMin = Math.round(props.data.main.temp_min);
+
+  const farenheitTemp = Math.round(props.data.main.temp);
+
+  return (
+    <div className="row">
+      <div className="col-12">
+        {/* <div className="card py-2 my-3"> */}
+          <div className="row data-row-hourly">
+            <div className="col data-col">
+              <h4 className="col-header">
+                {moment(_date).format("MMMM D YYYY")}
+              </h4>
+              <h5>
+                {props.data.day} at {moment(_date).format("HH:mm a")}
+              </h5>
+              <i className={_img}></i>
+              <p>{props.data.weather[0].description}</p>
+              <h5>Temperature: {farenheitTemp}°F</h5>
+              <p>
+                Minimum:{fahrenheitMin}°F and Maximum: {fahrenheitMax}°F
+              </p>
+            </div>
+          </div>
+        {/* </div> */}
+      </div>
+    </div>
+  );
+}
+
+export default HourlyForecast;
